dc6cb2a8afe3a3c57f5c5a8fc600131b11753488
Ashanti Haring <ashanti.haring@gmail.com>
2019-08-13 22:46:54 +0200

    configuration

--- s3.js
+++ s3.js
@@ -1,34 +1,40 @@
-const knox = require('knox');
-const fs = require('fs');
+const knox = require("knox");
+const fs = require("fs");
+const aws = require("aws-sdk");
 
 let secrets;
 
-if (process.env.NODE_ENV == 'production') {
+let s3 = new aws.S3({
+  accessKeyId: process.env.S3_KEY,
+  secretAccessKey: process.env.S3_SECRET
+});
+
+if (process.env.NODE_ENV == "production") {
   secrets = process.env; // in prod the secrets are environment variables
 } else {
-  secrets = require('./secrets'); // secrets.json is in .gitignore
+  secrets = require("./secrets"); // secrets.json is in .gitignore
 }
 const client = knox.createClient({
   key: secrets.AWS_KEY,
   secret: secrets.AWS_SECRET,
-  bucket: 'spicedling'
+  bucket: "spicedling"
 });
 
 module.exports.upload = (req, res, next) => {
   if (!req.file) {
-    console.log('multer failed');
+    console.log("multer failed");
     return res.sendStatus(500);
   }
   const s3Request = client.put(req.file.filename, {
-    'Content-Type': req.file.mimetype,
-    'Content-Length': req.file.size,
-    'x-amz-acl': 'public-read'
+    "Content-Type": req.file.mimetype,
+    "Content-Length": req.file.size,
+    "x-amz-acl": "public-read"
   });
 
   const readStream = fs.createReadStream(req.file.path);
   readStream.pipe(s3Request);
 
-  s3Request.on('response', s3Response => {
+  s3Request.on("response", s3Response => {
     if (s3Response.statusCode == 200) {
       next();
       fs.unlink(req.file.path, () => {});
