139ab906f3b37121de27a672985be20901b1da17
Ashanti Haring <ashanti.haring@gmail.com>
2019-08-14 15:54:17 +0200

    config

--- s3.js
+++ s3.js
@@ -4,6 +4,18 @@ const aws = require("aws-sdk");
 
 let secrets;
 
+//  new
+if (process.env.NODE_ENV == "production") {
+  secrets = {
+    AWS_ACCESS_KEYS: process.env.AWS_ACCESS_KEYS,
+    S3_KEY: process.env.S3_KEY
+  }
+} else {
+  secrets = require("./secrets");
+};
+
+// =========================================
+
 // let s3 = new aws.S3({
 //   accessKeyId: process.env.S3_KEY,
 //   secretAccessKey: process.env.S3_SECRET,
@@ -14,49 +26,44 @@ let secrets;
 // } else {
 //   secrets = require("./secrets"); // secrets.json is in .gitignore
 // }
-//  new
-if (process.env.NODE_ENV == “production”) {
-  secrets = {
-    AWS_ACCESS_KEYS: process.env.AWS_ACCESS_KEYS,
-    S3_KEY: process.env.S3_KEY
+
+// ===============================================
+
+
+// hab ich eingefügt (muss bucket auch aendern??)
+const client = knox.createClient({
+  key: secrets.S3_KEY,
+  secret: secrets.S3_SECRET,
+  bucket: secrets.S3_BUCKET_NAME
+});
+
+// const client = knox.createClient({
+//   key: secrets.AWS_KEY,
+//   secret: secrets.AWS_SECRET,
+//   bucket: "spicedling"
+// });
+
+module.exports.upload = (req, res, next) => {
+  if (!req.file) {
+    console.log("multer failed");
+    return res.sendStatus(500);
   }
-  else {
-    secrets = require(“. / secrets”);
+  const s3Request = client.put(req.file.filename, {
+    "Content-Type": req.file.mimetype,
+    "Content-Length": req.file.size,
+    "x-amz-acl": "public-read"
+  });
 
-    // hab ich eingefügt (muss bucket auch aendern??)
-    const client = knox.createClient({
-      key: secrets.S3_KEY,
-      secret: secrets.S3_SECRET,
-      bucket: secrets.S3_BUCKET_NAME
-    });
+  const readStream = fs.createReadStream(req.file.path);
+  readStream.pipe(s3Request);
 
-    // const client = knox.createClient({
-    //   key: secrets.AWS_KEY,
-    //   secret: secrets.AWS_SECRET,
-    //   bucket: "spicedling"
-    // });
-
-    module.exports.upload = (req, res, next) => {
-      if (!req.file) {
-        console.log("multer failed");
-        return res.sendStatus(500);
-      }
-      const s3Request = client.put(req.file.filename, {
-        "Content-Type": req.file.mimetype,
-        "Content-Length": req.file.size,
-        "x-amz-acl": "public-read"
-      });
-
-      const readStream = fs.createReadStream(req.file.path);
-      readStream.pipe(s3Request);
-
-      s3Request.on("response", s3Response => {
-        if (s3Response.statusCode == 200) {
-          next();
-          fs.unlink(req.file.path, () => {});
-        } else {
-          console.log("Error inS3Request:", s3Response.statusCode);
-          res.sendStatus(500);
-        }
-      });
-    }; //closes upload
+  s3Request.on("response", s3Response => {
+    if (s3Response.statusCode == 200) {
+      next();
+      fs.unlink(req.file.path, () => {});
+    } else {
+      console.log("Error inS3Request:", s3Response.statusCode);
+      res.sendStatus(500);
+    }
+  });
+}; //closes upload
